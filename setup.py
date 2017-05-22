a5dbca9a9162a7af6dccb7c63f19f32a73d5a2c7
erikvw <ew2789@gmail.com>
2017-05-22 15:10:28 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='cancer',
+    version='0.1.0',
+    author=u'Erik van Widenfelt',
+    author_email='ew2789@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/botswana-harvard/cancer',
+    license='GPL license, see LICENSE',
+    description='Botswana Prospective Cancer Cohort',
+    long_description=README,
+    zip_safe=False,
+    keywords='django cancer edc',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
