c89867699ae1cdd5df6eba783e18d61e263f30eb
aseokana <aseokana@bhp.org.bw>
2017-10-26 08:56:23 +0200

    setting the DEFAULT_PAGINATION_CLASS

--- cancer/settings.py
+++ cancer/settings.py
@@ -223,6 +223,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.LimitOffsetPagination',
+    )
 }
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
